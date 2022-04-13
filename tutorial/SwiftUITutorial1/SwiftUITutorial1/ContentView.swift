32d30c9cd8120c1c90e5410ad276a5c194bcb5c3
Sangwon <cold929@naver.com>
2022-04-13 22:28:34 +0900

    [ADD] .gitignore

---
+++ tutorial/SwiftUITutorial1/SwiftUITutorial1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITutorial1
+//
+//  Created by 신상원 on 2022/04/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tutorial/SwiftUITutorial1/SwiftUITutorial1/SwiftUITutorial1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorial1App.swift
+//  SwiftUITutorial1
+//
+//  Created by 신상원 on 2022/04/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorial1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
