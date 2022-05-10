f9179fc0ec72f491c909fed756575be987c0834c
Bran <cold929@naver.com>
2022-05-10 20:43:25 +0900

    EmotionDiary - First Commit

---
+++ tutorial/EmotionDiary/EmotionDiary/EmotionDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmotionDiaryApp.swift
+//  EmotionDiary
+//
+//  Created by Bran on 2022/05/10.
+//
+
+import SwiftUI
+
+@main
+struct EmotionDiaryApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
