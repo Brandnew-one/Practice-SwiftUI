80b5fdc3f8d55e57b222ba6dc1963355574a13e8
Bran <cold929@naver.com>
2022-05-15 01:43:46 +0900

    [First Commit] TrendMedia

---
+++ tutorial/TrendMedia/TrendMedia/TrendMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrendMediaApp.swift
+//  TrendMedia
+//
+//  Created by Bran on 2022/05/15.
+//
+
+import SwiftUI
+
+@main
+struct TrendMediaApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
