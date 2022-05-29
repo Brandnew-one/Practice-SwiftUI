32625605741de84d7db61403f8acd6d3b2854668
Bran <cold929@naver.com>
2022-05-29 21:58:01 +0900

    README

--- tutorial/ShoppingList/ShoppingList/Keyboard + Extension.swift
+++ tutorial/ShoppingList/ShoppingList/Keyboard + Extension.swift
@@ -11,7 +11,10 @@ import SwiftUI
 extension UIApplication {
   func hideKeyboard() {
     guard let window = windows.first else { return }
-    let tapRecognizer = UITapGestureRecognizer(target: window, action: #selector(UIView.endEditing))
+    let tapRecognizer = UITapGestureRecognizer(
+      target: window,
+      action: #selector(UIView.endEditing)
+    )
     tapRecognizer.cancelsTouchesInView = false
     tapRecognizer.delegate = self
     window.addGestureRecognizer(tapRecognizer)
@@ -19,7 +22,10 @@ extension UIApplication {
 }
 
 extension UIApplication: UIGestureRecognizerDelegate {
-  public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+  public func gestureRecognizer(
+    _ gestureRecognizer: UIGestureRecognizer,
+    shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer
+  ) -> Bool {
     return false
   }
 }
